085ecc38e6979086daa3b253e7325e7114565567
Tshiamo Justice Mothokho <rafxjay@gmail.com>
2026-08-05 19:31:54 +0200

    Added ValueObjects for entities and DTOs

---
+++ FootballAnalysis.Data/Application/Common/ValueObjects/MatchStats.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FootballAnalysis.Data.Application.Common.ValueObjects
+{
+    public class MatchStats
+    {
+        public int Possession { get; set; } = 0;
+        public int Corners { get; set; } = 0;
+        public int BigChances { get; set; } = 0;
+        public int BigChancesMissed { get; set; } = 0;
+    }
+
+    public class MatchShots
+    {
+        public int TotalShots { get; set; } = 0;
+        public int ShotsOnTarget { get; set; } = 0;
+        public int ShotsOffTarget { get; set; } = 0;
+        public int BlockedShots { get; set; } = 0;
+        public int HitWoodwork { get; set; } = 0;
+        public int ShotsInsideBox { get; set; } = 0;
+        public int ShotsOutsideBox { get; set; } = 0;
+    }
+
+    public class MatchExpectedGoals
+    {
+        public double XG { get; set; } = 0.0;
+        public double OpenPlayXG { get; set; } = 0.0;
+        public double SetPlayXG { get; set; } = 0.0;
+        public double NonPenaltyXG { get; set; } = 0.0;
+        public double XGOT { get; set; } = 0.0;
+    }
+
+    public class MatchPasses
+    {
+        public int Passes { get; set; } = 0;
+        public int AccuratePasses { get; set; } = 0;
+        public int OwnHalf { get; set; } = 0;
+        public int OppositionHalf { get; set; } = 0;
+        public int AccurateLongBalls { get; set; } = 0;
+        public int AccurateLongBallsPercentage { get; set; } = 0;
+        public int AccurateCrosses { get; set; } = 0;
+        public int AccurateCrossesPercentage { get; set; } = 0;
+        public int Throws { get; set; } = 0;
+        public int TouchesInOppositionBox { get; set; } = 0;
+        public int Offsides { get; set; } = 0;
+    }
+
+    public class MatchDiscipline
+    {
+        public int FoulsCommitted { get; set; } = 0;
+        public int YellowCards { get; set; } = 0;
+        public int RedCards { get; set; } = 0;
+    }
+
+    public class MatchDefence
+    {
+        public int Tackles { get; set; } = 0;
+        public int Interceptions { get; set; } = 0;
+        public int Blocks { get; set; } = 0;
+        public int Clearances { get; set; } = 0;
+        public int KeeperSaves { get; set; } = 0;
+    }
+
+    public class MatchDuels
+    {
+        public int DuelsWon { get; set; } = 0;
+
+        public int GroundDuelsWon { get; set; } = 0;
+        public int GroundDuelsWonPercentage { get; set; } = 0;
+
+        public int AerialDuelsWon { get; set; } = 0;
+        public int AerialDuelsWonPercentage { get; set; } = 0;
+
+        public int SuccessfulDribbles { get; set; } = 0;
+        public int SuccessfulDribblesPercentage { get; set; } = 0;
+    }
+
+    public class MatchAttackingZones
+    {
+        public int CenterAttack { get; set; } = 0;
+        public int LeftAttack { get; set; } = 0;
+        public int RightAttack { get; set; } = 0;
+    }
+
+    public class MatchAnalysis
+    {
+        public string TacticalNotes { get; set; } = "";
+        public string AnalystNotes { get; set; } = "";
+    }
+}
---
+++ FootballAnalysis.Data/Application/Common/ValueObjects/PlayerStats.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FootballAnalysis.Data.Application.Common.ValueObjects
+{
+    public class PlayerStats
+    {
+        public double FotmobRating { get; set; } = 6.0;
+        public double SofascoreRating { get; set; } = 6.0;
+        public int MinutesPlayed { get; set; } = 90;
+    }
+
+    public class PlayerAttack
+    {
+        public int Goals { get; set; } = 0;
+        public double XG { get; set; } = 0.0;
+        public double XGOT { get; set; } = 0.0;
+        public int TotalShots { get; set; } = 0;
+        public int ShotsOnTarget { get; set; } = 0;
+        public int TouchesInOppositionBox { get; set; } = 0;
+        public int BigChancesMissed { get; set; } = 0;
+        public int SuccessfulDribbles { get; set; } = 0;
+        public int DribblesAttempted { get; set; } = 0;
+    }
+    public class PlayerPasses
+    {
+        public int Touches { get; set; } = 0;
+        public int AccuratePasses { get; set; } = 0;
+        public int PassesAttempted { get; set; } = 0;
+        public int Assists { get; set; } = 0;
+        public double XA { get; set; } = 0.0;
+        public int ChancesCreated { get; set; } = 0;
+        public int PassesIntoFinalThird { get; set; } = 0;
+        public int AccurateCrosses { get; set; } = 0;
+        public int CrossesAttempted { get; set; } = 0;
+        public int AccurateLongBalls { get; set; } = 0;
+        public int LongBallsAttempted { get; set; } = 0;
+    }
+    public class PlayerDefence
+    {
+        public int DefensiveContributions { get; set; } = 0;
+        public int Tackles { get; set; } = 0;
+        public int Interceptions { get; set; } = 0;
+        public int Blocks { get; set; } = 0;
+        public int Recoveries { get; set; } = 0;
+        public int Clearance { get; set; } = 0;
+        public int HeadedClearances { get; set; } = 0;
+        public int DribbledPast { get; set; } = 0;
+    }
+    public class PlayerDuels
+    {
+        public int DuelsWon { get; set; } = 0;
+        public int DuelsLost { get; set; } = 0;
+        public int GroundDuelsWon { get; set; } = 0;
+        public int TotalGroundDuels { get; set; } = 0;
+        public int AerialDuelsWon { get; set; } = 0;
+        public int TotalAerialDuels { get; set; } = 0;
+    }
+    public class Goalkeepering
+    {
+        public int Saves { get; set; } = 0;
+        public int GoalsConceded { get; set; } = 0;
+        public double FacedxGOT { get; set; } = 0.0;
+        public double GoalsPrevented { get; set; } = 0.0;
+        public int ActedAsSweeper { get; set; } = 0;
+        public int HighClaim { get; set; } = 0;
+        public int LongBalls { get; set; } = 0;
+        public int AccurateLongBalls { get; set; } = 0;
+        public int Passes { get; set; } = 0;
+        public int AccuratePasses { get; set; } = 0;
+    }
+
+    public class PlayerDiscipline
+    {
+        public int YellowCards { get; set; } = 0;
+        public int RedCards { get; set; } = 0;
+        public int FoulsCommitted { get; set; } = 0;
+        public int WasFouled { get; set; } = 0;
+    }
+
+    public class PlayerAnalysis
+    {
+        public string PerformanceSummary { get; set; } = "";
+
+        public string AnalystNotes { get; set; } = "";
+    }
+}
