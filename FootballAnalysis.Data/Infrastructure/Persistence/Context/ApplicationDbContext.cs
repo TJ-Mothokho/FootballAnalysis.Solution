9aeaecaa9318c66f9a4f5ebc6776085a7aa3846a
Tshiamo Justice Mothokho <rafxjay@gmail.com>
2026-08-05 14:31:14 +0200

    Added Db context configurations

---
+++ FootballAnalysis.Data/Infrastructure/Persistence/Context/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using FootballAnalysis.Data.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FootballAnalysis.Data.Infrastructure.Persistence.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<TeamMatchStats> TeamMatchStats { get; set; }
+        public DbSet<Competition> Competitions { get; set; }
+        public DbSet<Season> Seasons { get; set; }
+        public DbSet<Match> Matches { get; set; }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<PlayerMatchStats> PlayerMatchStats { get; set; }
+
+    }
+}
