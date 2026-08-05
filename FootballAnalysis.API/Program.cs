3182853e0d9d643c9cd68fb851d17a5fa29df781
Tshiamo Justice Mothokho <rafxjay@gmail.com>
2026-08-05 16:11:03 +0200

    Added Scalar UI for API documentation

--- FootballAnalysis.API/Program.cs
+++ FootballAnalysis.API/Program.cs
@@ -1,3 +1,7 @@
+using FootballAnalysis.Data.Application;
+using FootballAnalysis.Data.Infrastructure;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,12 +10,16 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddApplication(builder.Configuration);
+builder.Services.AddInfrastructure(builder.Configuration);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
