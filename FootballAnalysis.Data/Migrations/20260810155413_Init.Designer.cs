00d411363a41ea783074b55b4a0a731ede22e6ec
Tshiamo Justice Mothokho <rafxjay@gmail.com>
2026-08-10 18:02:32 +0200

    Added migrations to cloud database

--- FootballAnalysis.Data/Migrations/20260810155413_Init.Designer.cs
+++ FootballAnalysis.Data/Migrations/20260810155413_Init.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FootballAnalysis.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20260810143632_Init")]
+    [Migration("20260810155413_Init")]
     partial class Init
     {
         /// <inheritdoc />
